e7fb92f00f5a9f4723b99e07e493c57402531747
Petar Spengler <petar.spengler@gmail.com>
2023-05-24 18:31:45 +0200

    promena paragrafa

--- src/Pocetna.jsx
+++ src/Pocetna.jsx
@@ -5,14 +5,14 @@ const Pocetna = () => {
     return (
         <div className="pocetna">
             <img className="pocetna-slika" src="https://mojepranje.rs/wp-content/uploads/2020/04/dubinsko-pranje-auta.jpg" alt="Naslovna slika" />
-            <p className="pocetna-tekst"> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque fugit iusto vitae tempora et provident fugiat. Quisquam ea reiciendis, quia nisi animi quibusdam aliquam eum eveniet suscipit molestiae, voluptates quis!</p>
+            <p className="pocetna-tekst"> Profesionalno dubinsko čišćenje automobila za besprekaran sjaj</p>
         
             <br /><br /><br />
 
-            <p className="pocetna-tekst"> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque fugit iusto vitae tempora et provident fugiat. Quisquam ea reiciendis, quia nisi animi quibusdam aliquam eum eveniet suscipit molestiae, voluptates quis!</p>
+            <p className="pocetna-tekst"> Dobrodošli u našu specijalizovanu firmu za dubinsko čišćenje automobila. Sa strašću prema detaljima i visokim standardima čistoće, posvećeni smo pružanju vrhunskih usluga čišćenja kako bismo vašem vozilu vratili besprekoran sjaj.</p>
             <br />
 
-            <p className="pocetna-tekst"> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque fugit iusto vitae tempora et provident fugiat. Quisquam ea reiciendis, quia nisi animi quibusdam aliquam eum eveniet suscipit molestiae, voluptates quis!</p>
+            <p className="pocetna-tekst"> Naš tim profesionalaca sa dugogodišnjim iskustvom koristi najmoderniju opremu i ekološki prihvatljive proizvode kako bi vaš automobil bio temeljito očišćen iznutra i izvana.</p>
 
 
 
